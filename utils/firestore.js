ca2214d8b568f3cc7bfe77be4efe19a3db936a8d
William Maber <maberwilliam@gmail.com>
2023-03-24 10:04:04 +0000

    code update

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,5 +1,5 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('path/to/serviceAccountKey.json');
+const admin = require("firebase-admin");
+const serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
